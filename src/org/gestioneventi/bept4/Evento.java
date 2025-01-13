9a4dd0cf8deb0a460a8ebee3f78e839aa33b8752
dianagiangregorio <diana.giangregorio@gmail.com>
2025-01-14 00:26:47 +0100

    feat: added methods

--- src/org/gestioneventi/bept4/Evento.java
+++ src/org/gestioneventi/bept4/Evento.java
@@ -1,6 +1,7 @@
 package org.gestioneventi.bept4;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Evento {
     private String titolo;
@@ -25,4 +26,72 @@ public class Evento {
         this.postiPrenotati = 0; //il numero di posti prenotati viene inizializzato a 0
     }
 
+    //getter e setter: titolo e data sono sia in lettura che in scrittura; posti totali e prenotati sono solo in lettura.
+
+    public String getTitolo(){
+        return this.titolo;
+    }
+
+    public LocalDate getDate(){
+        return this.data;
+    }
+
+    public int getPostiTotali(){
+        return this.postiTotali;
+    }
+
+    public int getPostiPrenotati(){
+        return this.postiPrenotati;
+    }
+
+    public void setTitolo(String titolo){
+        this.titolo = titolo;
+    }
+
+    public void setData(LocalDate data){
+        this.data = data;
+        // if (data.isAfter(LocalDate.now())) {
+        //     this.data = data;            
+        // }
+        // else {
+        //     System.out.println("La data inserita non è corretta.");
+        // }
+    }
+
+    //metodo per prenotare un post ad un evento
+    public void prenota(){
+        if (data.isBefore(LocalDate.now())) {
+            System.out.println("L'evento è già passato.");
+        }
+        else if (postiPrenotati >= postiTotali) {
+            System.out.println("Non ci sono posti disponibili.");
+        }
+        else{
+            postiPrenotati ++;
+            System.out.println("Prenotazione effeutata.");
+        }
+    }
+
+    public void disdici(){
+        if (data.isBefore(LocalDate.now())) {
+            System.out.println("L'evento è già passato.");
+        }
+        else if (postiPrenotati <= 0) {
+            System.out.println("Non ci sono prenotazioni per questo evento.");
+        }
+        else{
+            postiPrenotati --;
+            System.out.println("La tua prenotazione è stata disdetta.");
+        }
+    }
+
+    public String dataFormattata(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd LLL uuu");
+        return data.format(formatter);
+    }
+
+    @Override
+    public String toString(){
+        return "Data evento: " + dataFormattata() + " - Evento: " + titolo;
+    }
 }
